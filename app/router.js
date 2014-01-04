3527d058a0642dfd8e2f08e9fda0e13915f94dcb
Henrik Pettersen <henrik@sparklingideas.co.uk>
2014-01-04 08:12:32 +0000

    changed navigation

--- app/router.js
+++ app/router.js
@@ -4,10 +4,10 @@ Router.map(function() {
   this.resource('refsets', {path:'/'});
   this.resource('create',  {path:'/create'}, function(){});
   this.resource('refset',  {path:'/:publicId'}, function(){
-    this.route('concepts',  {path:'concepts'});
     this.resource('plan',  {path:'plan'}, function(){
       this.route('edit',  {path:'edit'});
       this.route('show',  {path:'show'});
+      this.route('concepts',  {path:'concepts'});
     });
     this.resource('snapshots',  {path:'snapshots'});
     this.resource('snapshot',  {path:'snapshot'}, function(){
