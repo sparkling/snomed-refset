f8ac5cae354482cb8d3efe961a23ac2d5fd310ec
Henrik Pettersen <henrik@sparklingideas.co.uk>
2014-01-07 05:36:17 +0000

    temp hack

--- app/helpers/concept-link.js
+++ app/helpers/concept-link.js
@@ -1,4 +1,4 @@
 export default Ember.Handlebars.makeBoundHelper(function(concept) {
-  var linkUrl='http://browser.snomedtools.com/version/1/concept/' + concept.get('id');
+  var linkUrl='https://browser.snomedtools.com/version/1/concept/' + concept.get('id');
   return new window.Handlebars.SafeString("<a href='" + linkUrl + "' target='_blank'>" + concept.title + "</a>");
 });
