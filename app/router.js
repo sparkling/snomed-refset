bb59f25ca4e5093849f38e41e09447e999c5b488
Henrik Pettersen <henrik@sparklingideas.co.uk>
2014-04-17 15:29:08 +0700

    Going to a refset's url (e.g. https://refset.snomedtools.com/anothertest) only gives a blank page. Should point to members maybe? https://jira.ihtsdotools.org/browse/RMT-11

--- app/router.js
+++ app/router.js
@@ -4,12 +4,12 @@ Router.map(function() {
   this.resource('refsets', {path:'/'});
   this.resource('create',  {path:'/create'}, function(){});
   this.resource('refset',  {path:'/:publicId'}, function(){
+    this.resource('members',   {path:''});
     this.resource('import',    {path:'import'}, function(){
       this.route('file',         {path:'file'});
       this.route('search',       {path:'search'});
       this.route('refset',       {path:'refset'});
-    });
-    this.resource('members',   {path:'members'});
+    });    
     this.resource('versions',  {path:'versions'});
     this.resource('version',   {path:'version/:versionPublicId'});
     this.resource('releases',  {path:'releases'});
