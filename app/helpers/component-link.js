546b6e07b97e606eebf3033b9f799209a3392ee9
Henrik Pettersen <henrik@sparklingideas.co.uk>
2014-04-21 01:10:33 +0700

    Added flag to avoid t&c popup.
    Acceptance of terms every time
    https://github.com/IHTSDO/snomed-interaction-components/issues/14

--- app/helpers/component-link.js
+++ app/helpers/component-link.js
@@ -1,4 +1,4 @@
 export default Ember.Handlebars.makeBoundHelper(function(component) {
-  var linkUrl = 'http://browser.ihtsdotools.org/index.html?perspective=full&conceptId1=' + component.id + '&diagrammingMarkupEnabled=true';
+  var linkUrl = 'http://browser.ihtsdotools.org/index.html?perspective=full&conceptId1=' + component.id + '&diagrammingMarkupEnabled=true&acceptLicense=true';
   return new window.Handlebars.SafeString('<a href="' + linkUrl + '" target="_blank">' + component.title + '</a>');
 });
